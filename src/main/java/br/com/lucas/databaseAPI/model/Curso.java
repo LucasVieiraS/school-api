9408d743c8a92fc92ca5ca541adfab34e9d59457
Lucas <lucasvieirasilva.lpta@gmail.com>
2022-08-08 07:48:54 -0300

    new: equals + hashCode method

--- src/main/java/br/com/lucas/databaseAPI/model/Curso.java
+++ src/main/java/br/com/lucas/databaseAPI/model/Curso.java
@@ -1,9 +1,23 @@
 package br.com.lucas.databaseAPI.model;
 
+import java.util.Objects;
+
 public class Curso {
 
     private Long id;
 
     private String nomeCurso;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Curso curso = (Curso) o;
+        return id.equals(curso.id) && nomeCurso.equals(curso.nomeCurso);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nomeCurso);
+    }
 }
