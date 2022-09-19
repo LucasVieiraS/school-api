f6cddf90cf53f34d4f6c25ec70025c20440dcbc2
Lucas <lucasvieirasilva.lpta@gmail.com>
2022-09-19 08:45:18 -0300

    new: added fk to aluno->curso + inserts
    FK_Aluno_Curso

--- src/main/resources/db/migration/V002__Criar_Tabela_Aluno.sql
+++ src/main/resources/db/migration/V002__Criar_Tabela_Aluno.sql
@@ -1,6 +1,33 @@
 CREATE TABLE IF NOT EXISTS aluno (
     id BIGINT NOT NULL AUTO_INCREMENT,
-    nomealuno VARCHAR(100),
+    nomealuno VARCHAR(150),
     cursoid BIGINT NOT NULL,
     PRIMARY KEY(id)
 );
+
+ALTER TABLE aluno
+ADD CONSTRAINT FK_Aluno_Curso
+FOREIGN KEY(cursoid)
+REFERENCES curso(id);
+
+INSERT INTO Aluno(nomealuno, cursoid)
+VALUES  ("João", 1),
+        ("Felipe", 2),
+        ("Carlos", 3),
+        ("Joana", 4),
+        ("Maria", 5),
+        ("Arthur", 6),
+        ("João Erik", 2),
+        ("Arthur", 5),
+        ("Isabelli", 3),
+        ("Arianny", 6),
+        ("Cristian", 3),
+        ("Lucas", 1),
+        ("Bidu", 2),
+        ("Marco", 1);
+
+/* VISUALIZATION/TEST INSERTS
+   SELECT nomealuno 'Nome Aluno', nomecurso 'Curso'
+   FROM aluno
+   JOIN curso ON curso.id = aluno.cursoid;
+*/
