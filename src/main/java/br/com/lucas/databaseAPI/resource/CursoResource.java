c0eb52fac05d96c1c647143c5138a7c3f57a5196
Lucas <79713436+LucasVieiraS@users.noreply.github.com>
2022-11-01 21:56:30 -0300

    new: rest controller mapping

--- src/main/java/br/com/lucas/databaseAPI/resource/CursoResource.java
+++ src/main/java/br/com/lucas/databaseAPI/resource/CursoResource.java
@@ -1,8 +1,21 @@
 package br.com.lucas.databaseAPI.resource;
 
+import br.com.lucas.databaseAPI.model.Curso;
+import br.com.lucas.databaseAPI.repository.CursoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 @RestController
+@RequestMapping("cursos")
 public class CursoResource {
+
+    @Autowired
+    private CursoRepository cursoRepository;
+
+    public List<Curso> listarTodosCursos() {
+      
+    }
 
 }
